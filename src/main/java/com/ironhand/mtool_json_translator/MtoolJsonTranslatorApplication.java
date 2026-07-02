69662f7856ff96493a5d9216ac0363fb640708c9
Ironhand-H <ziyang.h.huang@gmail.com>
2026-07-02 20:11:12 +0200

    feat: Initialize the first SpringBoot project generated by the template

---
+++ src/main/java/com/ironhand/mtool_json_translator/MtoolJsonTranslatorApplication.java
@@ -0,0 +1,13 @@
+package com.ironhand.mtool_json_translator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtoolJsonTranslatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtoolJsonTranslatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ironhand/mtool_json_translator/MtoolJsonTranslatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhand.mtool_json_translator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtoolJsonTranslatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
